062dfa942f5a122dcfa9e087fb2a6051f6ea841e
swaybailey <49054220+swaybailey@users.noreply.github.com>
2026-01-22 05:21:53 -0600

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,5 @@
+// auth.js  (private — do not commit)
+window.VM_CONFIG = {
+  PASSWORD: "testpassword",
+  SHEET_URL: "https://docs.google.com/spreadsheets/d/1HRd1uKlvqqRveJN7SCdYh9IOu8cU0ygRaMYEG3XL6eQ/edit?gid=671174622"
+};
