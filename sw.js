b1c6fd19d90430e903b158f5be7fed0d11cf84c2
swaybailey <49054220+swaybailey@users.noreply.github.com>
2026-01-28 01:33:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
+const CACHE_NAME = 'visitor-magic-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/addcontact.html',
+  '/manifest.json',
+  '/app-icon192.jpg',
+  '/app-icon.jpg',
+  // Add any other static assets you want cached
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
 self.addEventListener('fetch', event => {
-  // Always go to the network
-  return;
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
